28d3035c9a846de3f84764e7cbdc0a2ea8f4cfdd
angadsingh21 <angadsingh5@ontariotechu.net>
2024-03-20 06:58:51 -0400

    ibapi session incomplete

---
+++ sessions/ibapi_session.py
@@ -0,0 +1,103 @@
+# ! [socket_declare]
+class TestClient(EClient):
+    def __init__(self, wrapper):
+        EClient.__init__(self, wrapper)
+        # ! [socket_declare]
+
+        # how many times a method is called to see test coverage
+        self.clntMeth2callCount = collections.defaultdict(int)
+        self.clntMeth2reqIdIdx = collections.defaultdict(lambda: -1)
+        self.reqId2nReq = collections.defaultdict(int)
+        self.setupDetectReqId()
+
+    def countReqId(self, methName, fn):
+        def countReqId_(*args, **kwargs):
+            self.clntMeth2callCount[methName] += 1
+            idx = self.clntMeth2reqIdIdx[methName]
+            if idx >= 0:
+                sign = -1 if 'cancel' in methName else 1
+                self.reqId2nReq[sign * args[idx]] += 1
+            return fn(*args, **kwargs)
+
+        return countReqId_
+
+    def setupDetectReqId(self):
+
+        methods = inspect.getmembers(EClient, inspect.isfunction)
+        for (methName, meth) in methods:
+            if methName != "send_msg":
+                # don't screw up the nice automated logging in the send_msg()
+                self.clntMeth2callCount[methName] = 0
+                # logging.debug("meth %s", name)
+                sig = inspect.signature(meth)
+                for (idx, pnameNparam) in enumerate(sig.parameters.items()):
+                    (paramName, param) = pnameNparam # @UnusedVariable
+                    if paramName == "reqId":
+                        self.clntMeth2reqIdIdx[methName] = idx
+
+                setattr(TestClient, methName, self.countReqId(methName, meth))
+
+                # print("TestClient.clntMeth2reqIdIdx", self.clntMeth2reqIdIdx)
+
+
+# ! [ewrapperimpl]
+class TestWrapper(wrapper.EWrapper):
+    # ! [ewrapperimpl]
+    def __init__(self):
+        wrapper.EWrapper.__init__(self)
+
+        self.wrapMeth2callCount = collections.defaultdict(int)
+        self.wrapMeth2reqIdIdx = collections.defaultdict(lambda: -1)
+        self.reqId2nAns = collections.defaultdict(int)
+        self.setupDetectWrapperReqId()
+
+    # TODO: see how to factor this out !!
+
+    def countWrapReqId(self, methName, fn):
+        def countWrapReqId_(*args, **kwargs):
+            self.wrapMeth2callCount[methName] += 1
+            idx = self.wrapMeth2reqIdIdx[methName]
+            if idx >= 0:
+                self.reqId2nAns[args[idx]] += 1
+            return fn(*args, **kwargs)
+
+        return countWrapReqId_
+
+    def setupDetectWrapperReqId(self):
+
+        methods = inspect.getmembers(wrapper.EWrapper, inspect.isfunction)
+        for (methName, meth) in methods:
+            self.wrapMeth2callCount[methName] = 0
+            # logging.debug("meth %s", name)
+            sig = inspect.signature(meth)
+            for (idx, pnameNparam) in enumerate(sig.parameters.items()):
+                (paramName, param) = pnameNparam # @UnusedVariable
+                # we want to count the errors as 'error' not 'answer'
+                if 'error' not in methName and paramName == "reqId":
+                    self.wrapMeth2reqIdIdx[methName] = idx
+
+            setattr(TestWrapper, methName, self.countWrapReqId(methName, meth))
+
+            # print("TestClient.wrapMeth2reqIdIdx", self.wrapMeth2reqIdIdx)
+
+
+
+def main():
+    try:
+        app = TestApp()
+        if args.global_cancel:
+            app.globalCancelOnly = True
+        # ! [connect]
+        app.connect("127.0.0.1", args.port, clientId=0)
+        # ! [connect]
+        print("serverVersion:%s connectionTime:%s" % (app.serverVersion(),
+                                                    app.twsConnectionTime()))
+
+        # ! [clientrun]
+        app.run()
+        # ! [clientrun]
+    except:
+        raise
+
+if __name__ == "__main__":
+    main()
